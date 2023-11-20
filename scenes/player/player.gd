01983c9586af272ef0b3a45b9568c024253ad7db
Henryk Krzyszczak <henryk.krzyszczak@intive.com>
2023-11-20 16:54:36 +0100

    Add tiles

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 var can_laser: bool = true
 var can_grenade: bool = true
 
-signal laser(pos)
-signal grenade(pos)
+signal laser(pos, direction)
+signal grenade(pos, direction)
 
 
 func _process(_delta):
@@ -16,20 +16,21 @@ func _process(_delta):
 	look_at(get_global_mouse_position())
 	
 	
+	var player_direction = (get_global_mouse_position() - position).normalized()
 	
 	if Input.is_action_pressed("primary action") and can_laser:
 		can_laser = false
 		$LaserReloadTimer.start()
 		var laser_markers = $ParticleStartMarkers.get_children()
 		var selected_laser = laser_markers[randi() % laser_markers.size()]
-		laser.emit(selected_laser.global_position)
+		laser.emit(selected_laser.global_position, player_direction)
 
 	if Input.is_action_pressed("secondary action") and can_grenade:
 		can_grenade = false
 		
 		$GrenadeReloadTimer.start()
 		var grenade_position = $ParticleStartMarkers/GunCenterMarker.global_position
-		grenade.emit(grenade_position)
+		grenade.emit(grenade_position, player_direction)
 
 
 func _on_timer_timeout():
